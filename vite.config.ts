a1eb0711ff14de1e0289f0a48b56302ee4da5329
Maxim Kuzin <iskait04@gmail.com>
2022-11-06 19:36:20 +0300

    Инициализация репозитория

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import svgLoader from "vite-svg-loader";
+import path from "path";
+
+import dns from "dns";
+dns.setDefaultResultOrder("verbatim");
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), svgLoader()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+});
